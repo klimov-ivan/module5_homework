ebd2cd50efd3eec40d747be980113628abc5c370
klimov-ivan <klimov.i.b@gmail.com>
2022-09-19 21:54:00 +0500

    Homework-5.5 Массивы и базовые методы

---
+++ module5_homework-5.5.js
@@ -0,0 +1,12 @@
+//Задание 5.
+
+let arr = [1, 2, 3, 4, 5, 6, 7];
+console.log(arr.length);
+
+for (let i = 0; i < arr.length; i++) {
+	console.log(arr[i]);
+}
+
+for (let arry of arr) {
+	console.log(arry);
+  }
